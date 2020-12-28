7a3508dfdf33d70e45666e7618afe21401c19ac7
Sling <62813361+criquimj@users.noreply.github.com>
2020-12-28 14:55:51 -0600

    another test of github pushiiiiiiing

--- script.js
+++ script.js
@@ -12,3 +12,4 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 const testingGit = 'Matt Criqui';
+const testingGitTwo = 'Kelsey Criqui';
