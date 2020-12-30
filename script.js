753fe4da38c11f30e50b8e8e1dbb3a9024bf4e4d
Sling <62813361+criquimj@users.noreply.github.com>
2020-12-30 12:46:23 -0600

    Add interactivity for adding workouts to map

--- script.js
+++ script.js
@@ -17,7 +17,32 @@ navigator.geolocation.getCurrentPosition(
     const { longitude } = position.coords;
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-    // https://www.google.com/maps/@33.3601968,-86.7897111,15z
+    const coords = [latitude, longitude];
+
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    map.on('click', function (mapEvent) {
+      const { lat, lng } = mapEvent.latlng;
+      console.log(mapEvent);
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+    });
   },
   function () {
     alert('Could not get your position');
